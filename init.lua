15b2a57f9c95fb52cc7ba8b120767bcfdbc9fbc1
Cvnds <cvnds@proton.me>
2026-02-07 19:27:45 -0300

    iniciar telescope no inico do proglrma

--- init.lua
+++ init.lua
@@ -8,3 +8,12 @@ require("core.keymaps")
 -- Chama o gestor de plugins (Lazy)
 require("core.lazy_init") -- Você pode mover o código do Lazy para cá também
 
+-- vim enter apra inicar o nvim em um comando ou tela especifica
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Verifica se você abriu o nvim puro (sem passar um arquivo como 'nvim main.lua')
+    if vim.fn.argc() == 0 then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
