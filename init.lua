27fd982e381e7e45366efe4b227846172fec745c
Cvnds <cvnds@proton.me>
2026-02-11 21:03:06 -0300

    inicia fzf no inicio da aplicacao

--- init.lua
+++ init.lua
@@ -10,10 +10,10 @@ require("core.lazy_init") -- Você pode mover o código do Lazy para cá também
 
 -- vim enter apra inicar o nvim em um comando ou tela especifica
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    -- Verifica se você abriu o nvim puro (sem passar um arquivo como 'nvim main.lua')
-    if vim.fn.argc() == 0 then
-      require("telescope.builtin").find_files()
-    end
-  end,
+	callback = function()
+		-- Verifica se você abriu o nvim puro (sem passar um arquivo como 'nvim main.lua')
+		if vim.fn.argc() == 0 then
+			require("telescope.builtin").find_files()
+		end
+	end,
 })
